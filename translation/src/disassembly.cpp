8bfd8a4a8612e9f63dd3fe6467f06dc4a1e8ae96
Peter Mackay <mackay.pete+git@gmail.com>
2017-03-14 09:01:02 +0000

    Use big endian mode.

--- translation/src/disassembly.cpp
+++ translation/src/disassembly.cpp
@@ -11,7 +11,8 @@ Disassembly disassemble(const void *binary, size_t binary_size) {
 	Disassembly disassembly;
 	disassembly.cs = Capstone(new csh(0), &close_capstone);
 	
-	const cs_err err = cs_open(CS_ARCH_PPC, CS_MODE_64, disassembly.cs.get());
+	const cs_mode mode = static_cast<cs_mode>(CS_MODE_64 | CS_MODE_BIG_ENDIAN);
+	const cs_err err = cs_open(CS_ARCH_PPC, mode, disassembly.cs.get());
 	if (err != CS_ERR_OK) {
 		return Disassembly();
 	}
