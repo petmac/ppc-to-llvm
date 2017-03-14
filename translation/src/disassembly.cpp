35e46e0e4fd8188f6cb0abdd67f569baf0178462
Peter Mackay <mackay.pete+git@gmail.com>
2017-03-14 09:14:45 +0000

    Disassemble instructions.

--- translation/src/disassembly.cpp
+++ translation/src/disassembly.cpp
@@ -2,20 +2,33 @@
 
 #include <capstone/capstone.h>
 
+#include <functional>
+
 static void close_capstone(csh *csp) {
 	cs_close(csp);
 	delete csp;
 }
 
 Disassembly disassemble(const void *binary, size_t binary_size) {
-	Disassembly disassembly;
-	disassembly.cs = Capstone(new csh(0), &close_capstone);
-	
+	const Capstone cs(new csh(0), &close_capstone);
 	const cs_mode mode = static_cast<cs_mode>(CS_MODE_64 | CS_MODE_BIG_ENDIAN);
-	const cs_err err = cs_open(CS_ARCH_PPC, mode, disassembly.cs.get());
+	const cs_err err = cs_open(CS_ARCH_PPC, mode, cs.get());
 	if (err != CS_ERR_OK) {
 		return Disassembly();
 	}
+	
+	const uint64_t address = 0x8000000; // TODO Pass load address.
+	cs_insn *unsafe_insn = nullptr;
+	const size_t insn_count = cs_disasm(*cs, static_cast<const uint8_t *>(binary), binary_size, address, 0, &unsafe_insn);
+	const std::function<void(cs_insn *)> free_instructions = [insn_count](cs_insn *insn) {
+		cs_free(insn, insn_count);
+	};
+	const Instructions insn(unsafe_insn, free_instructions);
+	
+	Disassembly disassembly;
+	disassembly.cs = cs;
+	disassembly.insn = insn;
+	disassembly.insn_count = insn_count;
 	
 	return disassembly;
 }
--- translation/src/disassembly.h
+++ translation/src/disassembly.h
@@ -2,10 +2,15 @@
 
 #include <memory>
 
+struct cs_insn;
+
 typedef std::shared_ptr<size_t> Capstone;
+typedef std::shared_ptr<cs_insn> Instructions;
 
 struct Disassembly {
 	Capstone cs;
+	Instructions insn;
+	size_t insn_count = 0;
 };
 
 Disassembly disassemble(const void *binary, size_t binary_size);
