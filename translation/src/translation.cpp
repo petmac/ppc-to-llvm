90e3ae01957233794b8dd15aff9f2bb2e897077a
Peter Mackay <mackay.pete+git@gmail.com>
2017-03-15 13:17:36 +0000

    Added compilation tests target.

--- translation/src/translation.cpp
+++ translation/src/translation.cpp
@@ -5,5 +5,8 @@
 std::string translate(const void *binary, size_t binary_size, uint64_t address) {
 	const Disassembly disassembly = disassemble(binary, binary_size, address);
 	
-	return "TODO TRANSLATE";
+	return
+	"define i32 @main() {\n"
+	"\tret i32 0\n"
+	"}\n";
 }
