6f5bb970d94452cf57b05e71d20f585d87e4c224
Peter Mackay <mackay.pete+git@gmail.com>
2017-03-23 22:12:47 +0000

    Refactoring.

--- translation/translate.cpp
+++ translation/translate.cpp
@@ -8,18 +8,24 @@
 
 using namespace llvm;
 
-Translation translate(const Disassembly &disassembly) {
-	Translation translation;
-	translation.context = std::make_shared<LLVMContext>();
-	LLVMContext &context = *translation.context;
-	translation.module = std::make_shared<Module>("", context);
+static void generate_main(LLVMContext &context, Module *module) {
 	Type *const void_type = Type::getVoidTy(context);
 	FunctionType *const function_type = FunctionType::get(void_type, false);
-	Function *const main = Function::Create(function_type, GlobalValue::InternalLinkage, "main", translation.module.get());
+	Function *const main = Function::Create(function_type, GlobalValue::InternalLinkage, "main", module);
 	BasicBlock *const entry = BasicBlock::Create(context, "", main);
 	IRBuilder<> ir(context);
 	ir.SetInsertPoint(entry);
 	ir.CreateRetVoid();
+}
+
+Translation translate(const Disassembly &disassembly) {
+	const std::shared_ptr<LLVMContext> context = std::make_shared<LLVMContext>();
+	const std::shared_ptr<Module> module = std::make_shared<Module>("", *context);
+	generate_main(*context, module.get());
+	
+	Translation translation;
+	translation.context = context;
+	translation.module = module;
 	
 	return translation;
 }
