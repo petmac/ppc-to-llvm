126d99c134e747d2e87a379e4ba916f27e8d6345
Peter Mackay <mackay.pete+git@gmail.com>
2017-03-23 22:34:51 +0000

    Move file loading functions to their own file.

---
+++ tests/files.cpp
@@ -0,0 +1,32 @@
+#include "files.h"
+
+#include <fstream>
+#include <sstream>
+
+std::vector<char> load_binary_file(const char *path) {
+	std::ifstream file(path, std::ios::binary);
+	if (file.fail()) {
+		throw std::runtime_error("File open failed.");
+	}
+	
+	file.seekg(0, std::ios::end);
+	const size_t buffer_size = file.tellg();
+	file.seekg(0);
+	
+	std::vector<char> buffer(buffer_size);
+	file.read(buffer.data(), buffer.size());
+	
+	return buffer;
+}
+
+std::string load_text_file(const char *path) {
+	std::ifstream file(path);
+	if (file.fail()) {
+		throw std::runtime_error("File open failed.");
+	}
+	
+	std::ostringstream buffer;
+	buffer << file.rdbuf();
+	
+	return buffer.str();
+}
--- tests/translation.cpp
+++ tests/translation.cpp
@@ -1,3 +1,5 @@
+#include "files.h"
+
 #include "ppc-to-llvm/disassemble.h"
 #include "ppc-to-llvm/disassembly.h"
 #include "ppc-to-llvm/translate.h"
@@ -5,34 +7,6 @@
 #include "gtest/gtest.h"
 
 #include <fstream>
-
-static std::vector<char> load_binary_file(const char *path) {
-	std::ifstream file(path, std::ios::binary);
-	if (file.fail()) {
-		throw std::runtime_error("File open failed.");
-	}
-	
-	file.seekg(0, std::ios::end);
-	const size_t buffer_size = file.tellg();
-	file.seekg(0);
-	
-	std::vector<char> buffer(buffer_size);
-	file.read(buffer.data(), buffer.size());
-	
-	return buffer;
-}
-
-static std::string load_text_file(const char *path) {
-	std::ifstream file(path);
-	if (file.fail()) {
-		throw std::runtime_error("File open failed.");
-	}
-	
-	std::stringstream buffer;
-	buffer << file.rdbuf();
-	
-	return buffer.str();
-}
 
 class Translation : public testing::TestWithParam<const char *> {
 public:
