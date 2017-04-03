d12d557f9dd23bfcfa61fe1266cebdfd1db06a02
Peter Mackay <mackay.pete+git@gmail.com>
2017-04-03 23:30:45 +0100

    Clean up tests.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,42 +1,56 @@
-#include "tests.h"
-
 #include "data.h"
+#include "state.h"
 
-void Tests::SetUp() {
-	const char *const data_name = std::get<0>(GetParam());
-	const size_t arch_bits = std::get<1>(GetParam());
-	const size_t address_bits = std::get<2>(GetParam());
-	
+#include "gtest/gtest.h"
+
+#include <dlfcn.h>
+
+typedef std::unique_ptr<void, std::function<void(void *)>> DLL;
+
+static DLL load_dll(const char *data_name, size_t arch_bits, size_t address_bits) {
 	std::ostringstream dll_path;
 	dll_path << "data/" << data_name << "-" << arch_bits << "-" << address_bits << ".dylib";
 	
-	dll = std::unique_ptr<void, std::function<void(void *)>>(dlopen(dll_path.str().c_str(), RTLD_NOW), dlclose);
-	ASSERT_TRUE(dll);
+	return DLL(dlopen(dll_path.str().c_str(), RTLD_NOW), dlclose);
 }
 
-TEST_P(Tests, matches_expected) {
-	const size_t arch_bits = std::get<1>(GetParam());
-	const size_t address_bits = std::get<2>(GetParam());
+static std::string test_name(const testing::TestParamInfo<const char *> &param) {
+	return param.param;
+}
+
+template <typename R, typename Address>
+static void test(const char *data_name, size_t arch_bits, size_t address_bits) {
+	typedef State<R, Address> State;
+	typedef void RunFn(State *state);
 	
-	if (arch_bits == 32) {
-		if (address_bits == 32) {
-			matches_expected<int32_t, uint32_t>();
-		} else if (address_bits == 64) {
-			matches_expected<int32_t, uint64_t>();
-		} else {
-			FAIL();
-		}
-	} else if (arch_bits == 64) {
-		if (address_bits == 32) {
-			matches_expected<int64_t, uint32_t>();
-		} else if (address_bits == 64) {
-			matches_expected<int64_t, uint64_t>();
-		} else {
-			FAIL();
-		}
-	} else {
-		FAIL();
-	}
+	const DLL dll = load_dll(data_name, arch_bits, address_bits);
+	ASSERT_TRUE(dll);
+	
+	RunFn *const run = reinterpret_cast<RunFn *>(dlsym(dll.get(), "run"));
+	ASSERT_NE(run, nullptr);
+	
+	State state = {};
+	state.pc = 0x80000000; // TODO Get this from somewhere.
+	(*run)(&state);
 }
 
-INSTANTIATE_TEST_CASE_P(, Tests, testing::Combine(testing::ValuesIn(data_names), testing::Values(32, 64), testing::Values(32, 64)));
+class Tests : public testing::TestWithParam<const char *> {
+};
+
+TEST_P(Tests, ppc32_addr32) {
+	test<int32_t, uint32_t>(GetParam(), 32, 32);
+}
+
+TEST_P(Tests, ppc32_addr64) {
+	test<int32_t, uint64_t>(GetParam(), 32, 64);
+}
+
+TEST_P(Tests, ppc64_addr32) {
+	test<int64_t, uint32_t>(GetParam(), 64, 32);
+}
+
+TEST_P(Tests, ppc64_addr64) {
+	test<int64_t, uint64_t>(GetParam(), 64, 64);
+}
+
+INSTANTIATE_TEST_CASE_P(, Tests, testing::ValuesIn(data_names), test_name);
